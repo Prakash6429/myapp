cce9a8218fc2aeb0eebbfbfcef1bde822b898175
Prakash <rajprakash19.may@gmail.com>
2022-06-21 14:58:17 +0530

    Enter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+let name = "Harry";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <img src="" alt="" />
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur exercitationem totam natus velit nihil dicta cum eius, suscipit sed molestias magni nulla laudantium?
+      </p>
     </div>
+    </>
   );
 }
 
